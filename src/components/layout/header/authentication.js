cefce8c8d58349b538b6fcd32946a07d42611412
AlexPurhalo <alexpurhalo@gmail.com>
2017-02-21 20:55:41 +0200

    refactoring-of-header

--- src/components/layout/header/authentication.js
+++ src/components/layout/header/authentication.js
@@ -14,31 +14,11 @@ class Authentication extends Component {
 	constructor() {
 		super();
 
-		this.state = {
-			signUp: true, signIn: false,
-			username: '', password: '',
-			errors: []
-		};
-
-		this.activeSignInClick = this.activeSignInClick.bind(this);
-		this.activeSignUpClick = this.activeSignUpClick.bind(this);
-
-		this.handleChangeUsername = this.handleChangeUsername.bind(this);
-		this.handleChangePassword = this.handleChangePassword.bind(this);
-		this.sendForm = this.sendForm.bind(this);
-		this.signOutClick = this.signOutClick.bind(this);
+		this.state = { signUp: true, signIn: false, username: '', password: '', errors: [] };
 	}
 
-	// Changes Registration booleans
-	activeSignInClick() { this.setState({ signIn: true, signUp: false }) }
-	activeSignUpClick() { this.setState({ signUp: true, signIn: false }) }
-
-	// handleChanges data from inputs
-	handleChangeUsername(e) { this.setState({ username: e.target.value }) }
-	handleChangePassword(e) { this.setState({ password: e.target.value }) }
-
 	// Sends form to back-end
-	sendForm(e) {
+	sendForm = (e) => {
 		e.preventDefault();
 
 		if (this.state.username.length === 0 || this.state.password.length === 0) {
@@ -50,11 +30,7 @@ class Authentication extends Component {
 				this.props.postSession(this.state.username, this.state.password);
 			}
 		}
-	}
-
-	signOutClick() {
-		this.props.destroySession();
-	}
+	};
 
 	renderAuthForm() {
 		return (
@@ -63,7 +39,7 @@ class Authentication extends Component {
 					<li className="inline-block">
 						<div
 							className={`registration-title sign-up-title ${this.state.signUp ? 'active-type' : 'disabled-type'}`}
-							onClick={this.activeSignUpClick}>
+							onClick={e => this.setState({ signUp: true, signIn: false })}>
 							SignUp
 						</div>
 					</li>
@@ -73,20 +49,20 @@ class Authentication extends Component {
 					<li className="inline-block">
 						<div
 							className={`registration-title sign-in-title ${this.state.signIn ? 'active-type' : 'disabled-type'}`}
-							onClick={this.activeSignInClick}>
+							onClick={e => this.setState({ signIn: true, signUp: false })}>
 							Sign In
 						</div>
 					</li>
 					<li className="inline-block">
 						<input
-							onChange={this.handleChangeUsername}
+							onChange={e => this.setState({ username: e.target.value })}
 							type="text"
 							className={`form-field username ${this.state.signIn && 'active-sign-in'}`}
 							placeholder="Username"/>
 					</li>
 					<li className="inline-block">
 						<input
-							onChange={this.handleChangePassword}
+							onChange={e => this.setState({ password: e.target.value })}
 							type="password"
 							className={`form-field password ${this.state.signIn && 'active-sign-in'}`}
 							placeholder="Password"/>
@@ -117,7 +93,7 @@ class Authentication extends Component {
 					<li className="inline-block">
 						<span
 							className="sign-out-btn"
-							onClick={this.signOutClick}>
+							onClick={e => this.props.destroySession()}>
 							SignOut
 						</span>
 					</li>
